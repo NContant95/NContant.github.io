ae617699852dcf6c984a046730329f1476f476cd
NContant95 <noemie.contant@symbol-it.fr>
2024-01-25 15:16:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Navbar />
+  </div>
+  <body>
+  <img src = '/src/assets/img/header.jpeg' width="50%">
+  <h1>Marie Corrales, psychologue</h1>
+  </body>
+
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div class="navbar">
+    <div class="link">A propos de moi</div>
+    <div class="link">Public accueilli</div>
+    <div class="link">Prendre rendez-vous</div>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+};
+</script>
+
+<style>
+@import 'navbar.css';
+</style>
+
