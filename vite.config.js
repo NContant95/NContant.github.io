44c2954d27dcb2be44b2454ac05dadc54c07270b
NContant95 <noemie.contant@symbol-it.fr>
2024-01-25 21:05:09 +0100

    fixed version for firebase hosting

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/site-corrales/', // Remplacez 'nom-de-votre-repository' par le nom de votre référentiel GitHub
-
   plugins: [vue()],
-
   build: {
     outDir: 'dist',
-    assetsDir: '',
-    rollupOptions: {
-      input: {
-        main: './main-GV8ptCoH.js'
-      }
-    }
-  }
-})
+  },
+});
