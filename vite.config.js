38fbd2a9231eb48f0ab92e6067bac01b779e49ff
NContant95 <121037169+NContant95@users.noreply.github.com>
2024-01-25 15:57:04 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default {
+export default defineConfig({
+  base: '/site-corrales/', // Remplacez 'nom-de-votre-repository' par le nom de votre référentiel GitHub
+
+  plugins: [vue()],
+
   build: {
-    outDir: 'dist', // spécifie le répertoire de sortie du build
-    assetsDir: 'assets', // spécifie le répertoire où seront placés les fichiers statiques
+    outDir: 'dist',
+    assetsDir: '',
     rollupOptions: {
       input: {
-        main: './src/main.js' // spécifie le chemin d'accès vers votre main.js
+        main: './src/main.js'
       }
-    }
-  }
-}
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
