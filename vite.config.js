0132b7d7eac9ea74b2a4891698f55ffdd31723c9
NContant95 <121037169+NContant95@users.noreply.github.com>
2024-01-25 15:51:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+export default {
+  build: {
+    outDir: 'dist', // spécifie le répertoire de sortie du build
+    assetsDir: 'assets', // spécifie le répertoire où seront placés les fichiers statiques
+    rollupOptions: {
+      input: {
+        main: './src/main.js' // spécifie le chemin d'accès vers votre main.js
+      }
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
